f1630dd11192b44b9ae36ce041a860fdbf7ef5b9
ludoliv <ludovic111111@hotmail.fr>
2017-12-26 11:42:27 +0100

    redirection vers la page de l'utilisateur si le dernier est bien signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
