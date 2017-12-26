710d490800f1e13c4b3f1ea9f1244a69483137e3
ludoliv <ludovic111111@hotmail.fr>
2017-12-26 11:53:05 +0100

    ajout de ssl afin de sécuriser l'appli

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,10 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
