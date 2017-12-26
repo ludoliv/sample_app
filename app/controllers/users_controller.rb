01653c75e79120dc983ba95fdf757012d62aff0a
ludoliv <ludovic111111@hotmail.fr>
2017-12-26 11:17:27 +0100

    ajout d'un create temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+  
 end
