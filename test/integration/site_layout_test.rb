b7809501555496a992a1cdc173d2c1f9d2e41777
ludoliv <ludovic111111@hotmail.fr>
2017-12-26 09:35:12 +0100

    ajout des test nécessaire pour les liens

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home' # vérifie si le bon template est utilisé
+    assert_select "a[href=?]", root_path, count: 2 # On recherche si il y a bien 2 fois un lien vers root
+    assert_select "a[href=?]", help_path # On recherche si un lien vers help existe
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
 end
