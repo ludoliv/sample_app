4ccee09a9dfd892a0808d34156d77249688118f3
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 12:18:40 +0100

    ajout de admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:"Example User",email: "example@railstutorial.org",password:"foobar",password_confirmation: "foobar")
+User.create!(name:"Example User",email: "example@railstutorial.org",password:"foobar",password_confirmation: "foobar",admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
