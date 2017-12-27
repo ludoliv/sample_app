1a20b38549b5eebff3c9f680b893db0aeb639d95
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 09:57:41 +0100

    changement de user pour eviter la repetition de self

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,17 +8,19 @@ class User < ApplicationRecord
     validates :password, presence: true ,length: { minimum: 6 }
 
     # Returns the hash digest of the given string.
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
+    class << self
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
 
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
-
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
+    end
+    
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
