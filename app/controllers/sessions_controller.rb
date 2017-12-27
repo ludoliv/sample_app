c77417ab7e6d67792cac8ecafddd4d2d22a25dc8
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 08:46:48 +0100

    ajout d'un message flash (persiste donc a regler)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
