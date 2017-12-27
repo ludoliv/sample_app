572cd76a08d89c74707b0180d9badfad52d825c1
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 12:58:10 +0100

    ajout de l'activation pour les comptes tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:"Example User",email: "example@railstutorial.org",password:"foobar",password_confirmation: "foobar",admin: true)
+User.create!(name:"Example User",email: "example@railstutorial.org",password:"foobar",password_confirmation: "foobar",admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -15,5 +17,7 @@ User.create!(name:"Example User",email: "example@railstutorial.org",password:"fo
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
