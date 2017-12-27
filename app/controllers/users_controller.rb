6df813bec50f25e4689cb6576586945f20677993
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 10:57:24 +0100

    ajout update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 
   private
     def user_params
