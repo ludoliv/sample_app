b64dddd9d01f05983296fb29e8c82cf329b7cadd
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 11:17:54 +0100

    ajout de la condition d'edit d'un util (un util ne peut edit que sa propre page)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user=User.new
@@ -46,4 +47,8 @@ class UsersController < ApplicationController
       end
     end
 
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
 end
