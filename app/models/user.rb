8f8f4a2c3cca2028312d6917fb65cabf3961b7fb
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 16:45:10 +0100

    refactorisation du code pour eviter le code mal placé

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,4 +45,14 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+    def activate
+        update_attribute(:activated,    true)
+        update_attribute(:activated_at, Time.zone.now)
+    end
+
+     # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
 end
