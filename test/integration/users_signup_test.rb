2e6edcb8410e8b02ddcfe955c110c7ace98ce0e0
ludoliv <ludovic111111@hotmail.fr>
2017-12-27 13:27:28 +0100

    changement du test de signup car plus de redirection sur le site mais mail de confirmation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,7 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, params: { user: { name: "Example User",email: "user@example.com",password: "password",password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert is_logged_in?
   end
 end
