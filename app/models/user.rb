0d78aa8f450c8d7fb5dc760ad1de5b4bb30680a9
ludoliv <ludovic111111@hotmail.fr>
2017-12-28 14:44:24 +0100

    temps de vie de mdp reset mis a 2h

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,4 +64,8 @@ class User < ApplicationRecord
     def send_password_reset_email
         UserMailer.password_reset(self).deliver_now
     end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
 end
