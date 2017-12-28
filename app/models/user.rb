7530fb19b4bd010291ef314c8d17f9ab0646abbb
ludoliv <ludovic111111@hotmail.fr>
2017-12-28 17:06:20 +0100

    ajout des fonctions pour follow/unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,4 +74,18 @@ class User < ApplicationRecord
     def feed
         Micropost.where("user_id = ?", id)
     end
+
+    def follow(other_user)
+        following << other_user
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
+    end
 end
